1a83f7f31475f9aaf8e7838c36239e8ba455cfb2
ArnonAsquira <arnonafking2010@gmail.com>
2021-09-21 10:05:26 +0300

    trying to add the cat image unsuccesfuly due to cors policy

--- javascript for API task 19.9.2021.js
+++ javascript for API task 19.9.2021.js
@@ -6,6 +6,7 @@ let button = document.querySelector(".input-field>button");
     let resultDiv = document.getElementById("result-div");
     async function fetchFunc(myText){
         loadingStatus  = "loading..."
+        resultDiv.innerText = "";
         resultDiv.append(loadingStatus);
         let output =  await fetch("https://sentim-api.herokuapp.com/api/v1/" ,{
             method: "POST",
@@ -14,9 +15,11 @@ let button = document.querySelector(".input-field>button");
             },
             body : JSON.stringify({ "text":`${myText}` })
         });
+        /*et cat = await fetch (`https://http.cat/?[status_code]=200`)
+        console.log(cat);*/
         if(output.status > 400){
-            throw 'invalid request'
-        }
+            resultDiv.append("Error: invalid request")
+        }else{
         output = await output.json(); 
         output = {
             type: output.result.type,
@@ -36,9 +39,9 @@ let button = document.querySelector(".input-field>button");
              }else{
                 resultDiv.style.color = 'green'
              }
+            }
          }
-        
-          
+          //resultDiv.append(cat)
         }
     fetchFunc(input);
 };
